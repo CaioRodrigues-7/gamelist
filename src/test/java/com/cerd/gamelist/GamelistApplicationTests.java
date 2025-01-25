56f7307e2493eca5357ded064271ed82caecfbb5
Cerd7 <cerd7@proton.me>
2025-01-25 19:09:43 -0300

    first commit gameList

---
+++ src/test/java/com/cerd/gamelist/GamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cerd.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
