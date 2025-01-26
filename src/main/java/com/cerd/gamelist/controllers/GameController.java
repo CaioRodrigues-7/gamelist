508c7329c0ec2f0749cd256a0a2b204ad99183b3
Cerd7 <cerd7@proton.me>
2025-01-26 20:07:07 -0300

    Get game by id

--- src/main/java/com/cerd/gamelist/controllers/GameController.java
+++ src/main/java/com/cerd/gamelist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.cerd.gamelist.controllers;
 
+import com.cerd.gamelist.dto.GameDTO;
 import com.cerd.gamelist.dto.GameMinDTO;
 import com.cerd.gamelist.entities.Game;
 import com.cerd.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,13 @@ public class GameController
 {
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id)
+    {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll()
--- src/main/java/com/cerd/gamelist/services/GameService.java
+++ src/main/java/com/cerd/gamelist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.cerd.gamelist.services;
 
+import com.cerd.gamelist.dto.GameDTO;
 import com.cerd.gamelist.dto.GameMinDTO;
 import com.cerd.gamelist.entities.Game;
 import com.cerd.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,14 @@ public class GameService
     @Autowired
     public GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id)
+    {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll()
     {
         List<Game> result = gameRepository.findAll();
